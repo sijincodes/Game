25ef5d3fb013d6d4a2bf80d4caf44ceaf4ecbc80
sijincodes <sijinmathews03@gmail.com>
2022-10-31 15:34:45 +0100

    added array of images as enemies onto screen

--- js/enemy.js
+++ js/enemy.js
@@ -1,26 +1,55 @@
 //let container = document.querySelector('.container')
-let container= document.querySelector('.container')
-const enemy=document.createElement('img');
-let startBtn = document.querySelector('.btn')
-enemy.setAttribute('class','enemy');
-enemy.setAttribute('src','/Images/Cersei-Lannister-PNG-Image.png')
+let container = document.querySelector(".container");
+let containerHeight = container.offsetHeight;
+let containerWidth = container.offsetWidth;
 
-let containerHeight=container.offsetHeight;
-let containerWidth=container.offsetWidth;
-//console.log(containerHeight +' CH '+containerWidth)
-setInterval(() =>{
- let randomTop =Math.random()*(containerHeight-100)
-let randomLeft = Math.random()*(containerWidth-250)
-//let randomBottom=Math.random()*(containerHeight)
+let imgArray = [
+  "/Images/cersei.png",
+  "/Images/jaime3.png",
+  "/Images/joffery2.png",
+  "/Images/little-finger.png",
+  "/Images/ramsay1.png",
+  "/Images/tyrion1-img.png",
+];
 
-enemy.style.position='absolute'
-enemy.style.top=randomTop+ 'px'
-enemy.style.left=randomLeft+'px'
-//enemy.style.bottom=randomBottom+'px'
+let startBtn = document.querySelector(".btn");
+imgArray.forEach((elm) => {
+  //const enemy = document.createElement("img");
+  let enemy = document.createElement("img");
+  enemy.setAttribute("class", "enemy");
+  //enemy.setAttribute("src", "/Images/tyrion1-img.png");
+  enemy.setAttribute("src", elm);
+//})
+  setInterval(() => {
+    let randomTop = Math.random() * (containerHeight - 100);
+    let randomLeft = Math.random() * (containerWidth - 250);
+    //let randomBottom=Math.random()*(containerHeight)
 
-},1000)
+    enemy.style.position = "absolute";
+    enemy.style.top = randomTop + "px";
+    enemy.style.left = randomLeft + "px";
+    //enemy.style.bottom=randomBottom+'px'
+  }, 1000);
+//});
+window.addEventListener('click', elm =>{
+    console.log(elm.path[0])
+if(elm.path[0]==document.querySelector('.btn')){
+  bloodspot.style.top=50+'px';
+    bloodspot.style.left=50+'px';
+   
+}
+else{
+    bloodspot.style.top= elm.pageY +'px';
+    bloodspot.style.left=elm.pageX +'px';
+}
 
-startBtn.addEventListener('click',() =>{
-container.appendChild(enemy)
-startBtn.innerText='SCORE: '+score
+    if(elm.target===enemy)
+    score++;
+    startBtn.innerText='SCORE: '+score
+
 })
+
+startBtn.addEventListener("click", () => {
+  container.appendChild(enemy);
+  startBtn.innerText = "SCORE: " + score;
+}); })
