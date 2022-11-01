a68841bdd45cb5b5943058c809d322123caa4320
sijincodes <sijinmathews03@gmail.com>
2022-11-01 09:23:53 +0100

    new images & remove enemy after being shot

--- js/enemy-game.js
+++ js/enemy-game.js
@@ -1,22 +1,134 @@
-// let myFileArray = [
-//   "/Images/Cersei-Lannister-PNG-Image.png",
-//   "/Images/Cersei-Lannister-PNG-Image.png",
-// ]; // picture names,, add more
-// let myImageArray = [];
-// let many;
+const canvas =  document.querySelector('#canvas') ;
 
-// function setup() {
-//   size(640, 360);
-//   many = myFileArray.length;
-//   myImageArray = new PImage[many]();
-//   for (let i = 0; i < many; i++) myImageArray[i] = loadImage(myFileArray[i]); // load all images// from sketchdir or data/
-//   frameRate(1);
-// }
 
-// function draw() {
-//   let idx = random(many); // select a random index number
-//   let x = random(containerWidth - 150); // and a random picture position
-//   let y = random(containerHeight - 150);
-//   //println("i "+idx+" "+myFileArray[idx]);
-//   image(myImageArray[idx], x, y); // and show it
-// }
+canvas.width = window.innerWidth ;
+canvas.height = window.innerHeight ;
+
+window.addEventListener('resize', () => {
+    canvas.width = window.innerWidth ;
+    canvas.height = window.innerHeight ;
+})
+let c = canvas.getContext('2d') ;
+
+
+class Circle {
+    constructor(x,y,dx,dy,raduis,color) {
+        this.x = x;
+        this.y = y;
+        this.dx = dx;
+        this.dy = dy;
+        this.raduis = raduis;
+        this.color = color ;
+    }
+    draw() {
+        c.beginPath() ;
+        c.arc(this.x,this.y,this.raduis,0,Math.PI * 2,false) ;
+        c.fillStyle =  this.color ;
+        c.fill() ;
+        c.closePath() ;
+        return this ;
+    }
+    update() {
+        // if((this.x - this.raduis) < 0) {
+        //     if(this.dx < 0) {
+        //         this.dx = -this.dx
+        //     }
+           
+
+        //         this.dx = this.dx ;
+
+        // }
+        if((this.x + this.raduis) > window.innerWidth || (this.x - this.raduis < 0)) {
+            this.dx = -this.dx ;
+        }
+        if((this.y + this.raduis) > window.innerHeight || (this.y - this.raduis < 0)) {
+            this.dy = -this.dy ;
+        }
+   
+        this.x += this.dx ;
+        this.y += this.dy ;
+    }
+    reverse() {
+        if(this.dx > 0 || this.dx < 0 ) {
+            this.dx *= -1 ;
+        }
+        if(this.dy > 0 || this.dy < 0 ) {
+            this.dy *= -1 ;
+        }
+        return this ;
+    }
+    zoom() {
+        this.raduis += 5 ;
+    }
+}
+
+
+function getDistance(x1,x2,y1,y2) {
+    let distance = Math.sqrt(Math.pow(x2 - x1 ,  2) + Math.pow(y2 - y1 ,2)) ;
+    return distance ; 
+}
+
+
+let c2 = new Circle(canvas.width - 40,canvas.height / 2 , 2  , 0 , 40) ;
+
+let  c1 = new Circle(40,canvas.height / 2 , 2 , 0 , 40) ;
+
+let mouse = {
+    x: undefined ,
+    y: undefined 
+}
+window.addEventListener('mousemove' , event => {  
+    mouse.x = event.x ;
+    mouse.y = event.y ;
+}) ;
+
+function mouseOverObj(x,y,raduis) {
+   return mouse.x - x > 0 && mouse.x - x < raduis && mouse.y - y > 0  && mouse.y - y < raduis ;
+}
+
+let circleArray = [] ;
+
+let colors = [
+    'blue',
+     'red' ,
+     'black',
+     'deeppink',
+      'orange',
+      'pink',
+]
+for(let i = 0 ; i < 200 ; i++) {
+    let raduis = Math.random() * 10 + 1;
+    let x = Math.random() * (window.innerWidth - raduis * 2) + raduis ;
+    let y = Math.random() * (window.innerHeight - raduis * 2) + raduis ;
+    let dx = Math.random() * 4 ;
+    let dy = Math.random() * 4 ;
+    let color = colors [Math.floor(Math.random() * colors.length )] ;
+    circleArray.push(new Circle(x,y,dx,dy,raduis,color)) ;
+
+}
+
+function animate() {
+  let myReq =  requestAnimationFrame(animate) ;
+    c.clearRect(0,0,window.innerWidth,window.innerHeight) ;
+
+    
+     circleArray.forEach(circle => {
+         circle.draw().update();
+        //  if(getDistance(circle.x,c2.x,circle.y,c2.y) < (c1.raduis + c2.raduis)) {
+        //         c1.reverse().update();
+        //         c2.reverse().update();
+        //  }
+     })
+
+    // if(mouseOverObj(c1.x,c1.y,c1.raduis)) {
+    //  c1.zoom();
+    // }
+    // if(mouseOverObj(c2.x,c2.y,c2.raduis)) {
+    //  c2.zoom();
+    // }
+  
+}
+
+animate();
+
+
--- js/enemy.js
+++ js/enemy.js
@@ -5,51 +5,54 @@ let containerWidth = container.offsetWidth;
 
 let imgArray = [
   "/Images/cersei.png",
-  "/Images/jaime3.png",
-  "/Images/joffery2.png",
-  "/Images/little-finger.png",
-  "/Images/ramsay1.png",
-  "/Images/tyrion1-img.png",
+  "/Images/jaime10.png",
+    "/Images/joffery3.png",
+    "/Images/little-finger.png",
+    "/Images/ramsey8.png",
+    "/Images/tyrion1-img.png",
 ];
 
 let startBtn = document.querySelector(".btn");
-imgArray.forEach((elm) => {
-  //const enemy = document.createElement("img");
+imgArray.forEach((elm, index) => {
+  // if(index<2){
   let enemy = document.createElement("img");
   enemy.setAttribute("class", "enemy");
-  //enemy.setAttribute("src", "/Images/tyrion1-img.png");
+
   enemy.setAttribute("src", elm);
-//})
+
   setInterval(() => {
     let randomTop = Math.random() * (containerHeight - 100);
     let randomLeft = Math.random() * (containerWidth - 250);
-    //let randomBottom=Math.random()*(containerHeight)
 
     enemy.style.position = "absolute";
     enemy.style.top = randomTop + "px";
     enemy.style.left = randomLeft + "px";
-    //enemy.style.bottom=randomBottom+'px'
   }, 1000);
-//});
-window.addEventListener('click', elm =>{
-    console.log(elm.path[0])
-if(elm.path[0]==document.querySelector('.btn')){
-  bloodspot.style.top=50+'px';
-    bloodspot.style.left=50+'px';
-   
-}
-else{
-    bloodspot.style.top= elm.pageY +'px';
-    bloodspot.style.left=elm.pageX +'px';
-}
 
-    if(elm.target===enemy)
-    score++;
-    startBtn.innerText='SCORE: '+score
+  window.addEventListener("click", (el) => {
+    console.log(el.target);
+    if (el.path[0] == document.querySelector(".btn")) {
+      bloodspot.style.top = 50 + "px";
+      bloodspot.style.left = 50 + "px";
+    } else {
+      bloodspot.style.top = el.pageY + "px";
+      bloodspot.style.left = el.pageX + "px";
+    }
 
-})
+    if (el.target === enemy) {
+      score++;
 
-startBtn.addEventListener("click", () => {
-  container.appendChild(enemy);
-  startBtn.innerText = "SCORE: " + score;
-}); })
+      //   imgArray = imgArray.filter((img) => {
+      //     img !== el.target.getAttribute("src");
+      //   });
+      container.removeChild(el.target);
+    }
+
+    startBtn.innerText = "SCORE: " + score;
+  });
+
+  startBtn.addEventListener("click", () => {
+    container.appendChild(enemy);
+    startBtn.innerText = "SCORE: " + score;
+  });
+});
