c45a680066629dd82b3df8ed4184683ce00068c2
sijincodes <sijinmathews03@gmail.com>
2022-11-01 19:53:05 +0100

    cleaned up code & added logic for lives counter

--- js/bloodspot.js
+++ js/bloodspot.js
@@ -1,21 +1,41 @@
-let bloodspot = document.querySelector('.bloodspot')
-let score=0;
+let bloodspot = document.querySelector(".bloodspot");
+let score = 0;
 
-// window.addEventListener('click', elm =>{
-//     console.log(elm.path[0])
-// if(elm.path[0]==document.querySelector('.btn')){
-//   bloodspot.style.top=50+'px';
-//     bloodspot.style.left=50+'px';
-   
-// }
-// else{
-//     bloodspot.style.top= elm.pageY +'px';
-//     bloodspot.style.left=elm.pageX +'px';
-// }
+window.addEventListener("click", (el) => {
+  console.log(el.target.getAttribute("src") + " el.target");
+  if (el.path[0] == startBtn) {
+    bloodspot.style.top = 10 + "px";
+    bloodspot.style.left = 250 + "px";
+    startBtn.innerText = "SCORE: " + score;
+    livesSpan.innerText = "LIVES: " + lives;
+  } else {
+    bloodspot.style.top = el.pageY + "px";
+    bloodspot.style.left = el.pageX + "px";
+    
+  }
 
-//     if(elm.target===enemy)
-//     score++;
-//     startBtn.innerText='SCORE: '+score
+  if (imgArray.includes(el.target.getAttribute("src"))) {
+    score++;
+    startBtn.innerText = "SCORE: " + score;
 
-// })
+    container.removeChild(el.target);
+  }
+  else {
+    livesSpan.innerText = "Lives: " + lives--;
+  }
+  //     console.log(el.target + "el target from not being === enemy")
+  //    lives--;
+  //    livesSpan.innerText="Lives: "+ lives;
+  //   //  console.log(lives + " from el.target=container")
 
+  // }
+});
+// code to push new enemies to screen --> when & how will this work?!?!?!? :'( :'(
+//   if(imgArray.length==0){
+//     offsetArr.forEach(offsetElm =>{
+
+//           imgArray.push(offsetElm)
+//           this.print();
+
+//       })
+//  }
--- js/enemy.js
+++ js/enemy.js
@@ -2,78 +2,47 @@
 let container = document.querySelector(".container");
 let containerHeight = container.offsetHeight;
 let containerWidth = container.offsetWidth;
+let lives = 4;
 
 let imgArray = [
   "/Images/cersei.png",
   "/Images/jaime10.png",
-    "/Images/joffery3.png",
-    // "/Images/little-finger.png",
-    // "/Images/ramsey8.png",
-    // "/Images/tyrion1-img.png"
+  "/Images/joffery3.png",
+  "/Images/little-finger.png",
+  "/Images/ramsey8.png",
+  "/Images/tyrion1-img.png",
 ];
 let offsetArr = [
-    "/Images/little-finger.png",
-    "/Images/ramsey8.png",
-    "/Images/tyrion1-img.png"
-]
+  "/Images/little-finger.png",
+  "/Images/ramsey8.png",
+  "/Images/tyrion1-img.png",
+];
 
 let startBtn = document.querySelector(".btn");
+let livesSpan = document.querySelector(".lives");
 
+function initiateGame() {
+  imgArray.forEach((elm) => {
+    let enemy = document.createElement("img");
+    enemy.setAttribute("class", "enemy");
 
-imgArray.forEach((elm) => {
-    
-   
-  let enemy = document.createElement("img");
-  enemy.setAttribute("class", "enemy");
+    enemy.setAttribute("src", elm);
 
-  enemy.setAttribute("src", elm);
+    setInterval(() => {
+      let randomTop = Math.random() * (containerHeight - 100);
+      let randomLeft = Math.random() * (containerWidth - 250);
 
-  setInterval(() => {
-    let randomTop = Math.random() * (containerHeight - 100);
-    let randomLeft = Math.random() * (containerWidth - 250);
+      enemy.style.position = "absolute";
+      enemy.style.top = randomTop + "px";
+      enemy.style.left = randomLeft + "px";
+    }, 1000);
 
-    enemy.style.position = "absolute";
-    enemy.style.top = randomTop + "px";
-    enemy.style.left = randomLeft + "px";
-  }, 1000);
-
-
-  window.addEventListener("click", (el) => {
-   // console.log(el.target);
-    if (el.path[0] == startBtn) {
-      bloodspot.style.top = 50 + "px";
-      bloodspot.style.left = 50 + "px";
-      startBtn.innerText = "SCORE: " + score;
-    } else {
-      bloodspot.style.top = el.pageY + "px";
-      bloodspot.style.left = el.pageX + "px";
-    }
-
-    if (el.target === enemy) {
-        
-      score++;
-      startBtn.innerText = "SCORE: " + score;
-      container.removeChild(el.target);
-     
-    }
-
-  });
-  // code to push new enemies to screen --> when & how will this work?!?!?!? :'( :'( 
-//   if(imgArray.length==0){
-//     offsetArr.forEach(offsetElm =>{
-     
-//           imgArray.push(offsetElm)
-//           this.print();
-    
-        
-//       })
-//  }
-
-  startBtn.addEventListener("click", () => {
     container.appendChild(enemy);
-    startBtn.innerText = "SCORE: " + score;
   });
+}
 
+startBtn.addEventListener("click", () => {
+  initiateGame();
+  startBtn.innerText = "SCORE: " + score;
+  //livesSpan.innerText="Lives: "+lives;
 });
- 
-
